7ae3dcffbd026aef32c5eb83778aaae218d93c36
Adam Craig and Ashley Maceli <ashley@>
2016-06-01 09:43:42 -0700

    add Keg object/model, add functionality for listing out names of each keg in the kegs array in AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,29 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-      <h1>Tap Room</h1>
+    <div class="container">
+      <h1>Turtle Tap Room</h1>
+      <h3 *ngFor="#keg of kegs">
+        {{ keg.name }}
+      </h3>
+    </div>
   `
 })
 export class AppComponent {
+  public kegs: Keg[];
+  constructor(){
+    this.kegs = [
+      new Keg("Marionberry Braggot", "Rogue", 7, 11.4),
+      new Keg("Mocha Porter", "Rogue", 5, 5.6),
+      new Keg("Juice Box", "Great Notion", 8, 8.2),
+      new Keg("Double Stack", "Great Notion", 4, 7.5)
+    ];
+  }
+}
 
+export class Keg {
+  public pintsLeft: number = 124;
+  constructor(public name: string, public brand: string, public price: number, public abv: number) {
+
+  }
 }
